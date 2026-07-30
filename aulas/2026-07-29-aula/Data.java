7901835c706efecd1be59351f10f3a2f5ba79be6
Daniel Bentes Perquim <lperquim@gmail.com>
2026-07-29 22:36:07 -0300

    adicionando aula do dia 29-07-2026

---
+++ aulas/2026-07-29-aula/Data.java
@@ -0,0 +1,65 @@
+public class Data {
+    void main(){
+        String disciplina = IO.readln("Entre com a sigla da disciplina: ").toUpperCase();
+        String[] dias= new String[5];
+
+        int i = 0;
+        boolean sair = false;
+
+        while (i < 5 && sair == false) {
+            String dia = IO.readln("Informe o dia da semana ou nao para encerrar:");
+            
+
+            // if (dia.equals("seg")) {
+
+            //     dias[i++] = dia;
+
+            // } else if (dia.equals("ter")){
+
+            //     dias[i++] = dia;
+                
+            // } else if (dia.equals("qua")){
+
+            //     dias[i++] = dia;
+                
+            // } else if (dia.equals("qui")){
+
+            //     dias[i++] = dia;
+                
+            // } else if (dia.equals("sex")){
+
+            //     dias[i++] = dia;
+                
+            // }
+            switch (dia) {
+                case "seg", "ter", "qua", "qui", "sex":
+                    dias[i++] = dia;
+                    break;
+                case "nao":
+                    sair = true;  
+                    break;  
+                default:
+                    IO.println("ERRO: Valor inválido. Entre com: seg, ter, qua, qui ou sex");
+                    break;
+            }
+            // else{
+            //     IO.println("ERRO: Valor inválido. Entre com: seg, ter, qua, qui ou sex");
+            // }
+        }
+        for (int j = 0; j < i; j++) {
+            
+            IO.println("Na disciplina " + disciplina + " você tem aulas nos seguintes dias: " + dias[j]);
+            
+        }
+    }
+    // Faça um programa que o usuário deve informar:
+    // - Sigla da Diciplina
+    // - Todos os dias da semana que tem aula dessa disciplina
+    // 
+    // Por fim imprima:
+    // Na Disciplina **** você tem aulas nos seguintes dias:
+    // - seg
+    // - qua
+    // - sex
+
+}
